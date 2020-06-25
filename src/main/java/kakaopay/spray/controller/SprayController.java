f26be309fd89032bb3fbbedaf5fa6afc1b66f72f
jisu <jisu@skoopmedia.co.kr>
2020-06-25 19:01:56 +0900

    컨트롤러, 헤더

--- src/main/java/kakaopay/spray/controller/SprayController.java
+++ src/main/java/kakaopay/spray/controller/SprayController.java
@@ -1,16 +1,35 @@
 package kakaopay.spray.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import sun.reflect.annotation.ExceptionProxy;
+
 
 @RestController
+@RequestMapping(value="/spray")
 public class SprayController {
 
-    @GetMapping(value="/index")
-    public ResponseEntity<?> index() {
+    /* 뿌리기 */
+    @PostMapping(value="")
+    public ResponseEntity<?> spray(@RequestHeader(value = "X-USER-ID") String userId) {
 
-        return new ResponseEntity<>("good", HttpStatus.OK);
+        return new ResponseEntity<>(userId, HttpStatus.OK);
+
+    }
+
+    /* 받기 */
+    @PutMapping(value="/test")
+    public ResponseEntity<?> receive() {
+
+        return new ResponseEntity<>("receive", HttpStatus.OK);
+    }
+
+    /* 조회 */
+    @GetMapping(value="/test")
+    public ResponseEntity<?> check() {
+
+        return new ResponseEntity<>("check", HttpStatus.OK);
     }
 }
